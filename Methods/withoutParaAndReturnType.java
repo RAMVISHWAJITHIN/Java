7ddb9589ad93d113779d45f7adf81c8d6e1687d6
Ramvishwajithin Porandla <ramvishwajithin@gmail.com>
2026-01-08 23:38:00 +0530

    8-jan-2026

---
+++ Methods/withoutParaAndReturnType.java
@@ -0,0 +1,17 @@
+
+public class withoutParaAndReturnType {
+    public static void m1() {
+        System.out.println("Hi-m1");
+    }
+
+    public static void m2() {
+        System.out.println("Bye-m2");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main method starts");
+        m1();
+        m2();
+        System.out.println("Main Method ends");
+    }
+}
